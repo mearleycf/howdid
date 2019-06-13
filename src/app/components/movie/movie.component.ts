99a7990ef6c57e94c740dacd5b9ae5f6e6bb6707
Mike Earley <mike@5sn1.com>
2019-06-13 12:16:07 -0400

    app/components/movie/movie.: adding movie model dummy data

--- src/app/components/movie/movie.component.ts
+++ src/app/components/movie/movie.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Movie } from './movie';
+
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
   styleUrls: ['./movie.component.scss']
 })
 export class MovieComponent implements OnInit {
+
+  public movie: Movie;
 
   constructor() { }
 
---
+++ src/app/components/movie/movie.ts
@@ -0,0 +1,27 @@
+export class Movie {
+    static movies: Movie[] = [
+        'movie_results', [
+            {
+              'adult': false,
+              'backdrop_path': '/6Bx6fsUqJDsBjDCgIMSnjN3ua6i.jpg',
+              'genre_ids': [
+                35,
+                10751
+              ],
+              'id': 18357,
+              'original_language': 'en',
+              'original_title': 'The Country Bears',
+              // tslint:disable-next-line:max-line-length
+              'overview': 'For Beary Barrington, The Country Bears\' young #1 fan, fitting in with his all-too-human family is proving im-paws-ible. When he runs away to find Country Bear Hall and his heroes, he discovers the venue that made them famous is near foreclosure. Beary hightails it over the river and through the woods to get the Bears in the Band back together for an all-out reunion concert to save Country Bear Hall.',
+              'poster_path': '/xyRugigU9xBlTzfoA7uuIQ6KXiY.jpg',
+              'release_date': '2002-07-26',
+              'title': 'The Country Bears',
+              'video': false,
+              'vote_average': 4.3,
+              'vote_count': 45,
+              'popularity': 3.712
+            }
+          ]
+    ];
+
+}
